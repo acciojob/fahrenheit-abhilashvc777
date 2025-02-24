132835007e17078bfbe56b0ee0c6398c4d8aa504
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-24 13:57:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function toFahrenheit(celsius) {
  // Write your code here
-	let res = celsius*9/5+32
+	let res = celsius*(9/5)+32
 	res = `${res}`
-	if(res.includes(".")){
-		res = res
+	if(res.includes(".")){ 
+		let a = res.split(".")
+		if(a[1].length>=2){
+		  res = a[0]+"."+a[1][0]+a[1][1]
+		}else{
+			res = a[0]+"."+a[1]
+		}
 	}else{
 		res = res+".00"
-	}
-	return res
+	} 
+return res
 }
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:"); 
-alert(toFahrenheit(Number(celsius)));
+alert(toFahrenheit(Number(celsius))); 
